b199431fb68aba37d0f5017d09f833de97e1261a
Pranamya <pranamyaks9@gmail.com>
2026-09-24 04:21:28 +0530

    Create selectionsort.cpp

---
+++ Sorting-Techniques/selectionsort.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int arr[]={67,89,34,56,21};
+    int n=5;
+
+    for(int i=0;i<n-1;i++)
+    {
+        int minIndex=i;
+
+        for(int j=i+1;j<n;j++)
+        {
+            if(arr[j]<arr[minIndex]){
+                minIndex=j;
+            }
+        }
+
+        swap(arr[i],arr[minIndex]);
+    }
+
+    for(int i=0;i<n;i++){
+        cout<<arr[i]<<" ";
+    }
+}
