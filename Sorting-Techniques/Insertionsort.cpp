0eaa3da435f533b20240825675fc637524e73873
Pranamya <pranamyaks9@gmail.com>
2026-09-24 04:22:05 +0530

    Create Insertionsort.cpp

---
+++ Sorting-Techniques/Insertionsort.cpp
@@ -0,0 +1,31 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    //int n,arr[n];
+
+    int arr[]={67,89,34,56,21};
+    int n=5;
+
+    for(int i=1;i<n;i++)
+    {
+        int key=arr[i];
+        int j=i-1;
+
+        while(j>=0 && arr[j]>key)
+        {
+            arr[j+1]=arr[j];
+            j--;
+        }
+
+        arr[j+1]=key;
+
+    }    
+
+        for(int i=0;i<n;i++)
+        {
+            cout<<arr[i]<<" ";
+        }
+
+}    
